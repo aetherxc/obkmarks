9c1604ce1c59fa4ec5ecc2736ef1c11d5b5d8c5b
aetherxc <ajjaatar.tech@gmail.com>
2021-08-15 20:45:55 +1000

    skeleton

--- obkmarks.py
+++ obkmarks.py
@@ -1,3 +1,16 @@
-open("pstuff.txt")
+import webbrowser
 
-#lol
+# create directory with the name bkmarks 
+# if a directory with name bkmarks doen't exist
+
+# then make a file in it
+
+stuffinfile = open("bkmarks/" + "url_list.txt")
+
+url = stuffinfile.readline()
+
+# print(url)
+
+# open urls in a webbrowser
+
+webbrowser.open_new_tab(url) 
