2596c87e35ef67449c5e659992660d2b53c97a89
aetherxc <ajjaatar.tech@gmail.com>
2021-08-16 18:39:16 +1000

    basic stuff but not fault-tolerent

--- obkmarks.py
+++ obkmarks.py
@@ -1,16 +1,35 @@
+import os
 import webbrowser
 
-# create directory with the name bkmarks 
-# if a directory with name bkmarks doen't exist
+def create_directory_file():
+    # create directory with the name bkmarks 
+    # if a directory with name bkmarks doesn't exist
+    if not(os.path.isdir("bkmarks")):
+        try:
+            os.mkdir("./bkmarks")
+            # create the file to store urls
+            urls_in_file = open("bkmarks/" + "url_list.txt", "w")
+            urls_in_file.write("www.google.com")
+        except OSError as error: 
+            print(error) 
+        urls_in_file.close()
 
-# then make a file in it
+def open_urls():
+    try:
+        # open the file containing the urls
+        urls_in_file = open("bkmarks/" + "url_list.txt")
+    except OSError() as e:
+        print(e)
+    else:
+        # open the urls on browser
+        for i in urls_in_file.readlines():
+            print("yo")
+            webbrowser.open_new_tab(i)
+    urls_in_file.close()
 
-stuffinfile = open("bkmarks/" + "url_list.txt")
+def main():
+    create_directory_file()
+    open_urls()
 
-url = stuffinfile.readline()
-
-# print(url)
-
-# open urls in a webbrowser
-
-webbrowser.open_new_tab(url) 
+if __name__ == "__main__":
+    main()
